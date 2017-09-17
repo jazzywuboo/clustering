39987e4b9d2c179d701f7c886e6c92e011e76aee
jazzywuboo <normanj2@vcu.edu>
2017-09-17 00:40:23 -0400

    Update vcluster_converter.pl

--- vcluster_converter.pl
+++ vcluster_converter.pl
@@ -2,76 +2,111 @@
 use strict;
 use warnings;
 use Cwd;
-
+use Data::Dumper;
 # takes in files containing cui-word2vec vectors and prints them in the format required for the vcluster program in CLUTO
-# usage: perl vcluster_converter.pl [source_dir] [dest_dir]
+# usage: perl vcluster_converter.pl [unformatted_dir] [vcluster_dir]
 
-my ($source_dir, $dest_dir) = GetDirs();
-my $sizeof_matrices = DetermineSizeofMatrices();
+my ($unformatted_dir, $vcluster_dir, $num_clusters) = GetDirs();
+my $input_filenames = SaveInputFilenames();
+my $sizeof_matrices = GetSizeofMatrices();
 my $matrices = ExtractMatrices();
+#my $cui_vector_lookup = SaveCuiVectorPairs();
+
 PrintVClusterMatrices($sizeof_matrices, $matrices);
+#RunVCluster();
+#MatchClusteredVectorsToCuis();
+
+sub PrintUsageNotes {
+	print "Usage:\tperl vcluster_converter.pl [unformatted_dir] [vcluster_dir] [num_clusters]\n";
+}
 
 sub GetDirs {
 	# gets source directory and destination directory from user
 	no warnings 'uninitialized';
 	my $base_dir = cwd();
-	my $source_dir = "$ARGV[0]";
-	my $dest_dir = "$ARGV[1]";
-	if (! (-e $source_dir) ){
-		print "$source_dir not found. Check source directory name.\n";
+	my $unformatted_dir = $ARGV[0];
+	my $vcluster_dir = $ARGV[1];
+	my $num_clusters = $ARGV[2];
+	if (-f $unformatted_dir){
+		print "Error: Input must be a directory.\n";
+		PrintUsageNotes();
 		exit
 	}
-	if (! (-e $dest_dir)){
-		mkdir "$base_dir/$dest_dir", 0755;
+	if (! (-e $unformatted_dir) ){
+		print "Error: $unformatted_dir not found. Check source directory name.\n";
+		PrintUsageNotes();
+		exit
 	}
-	$source_dir = "$base_dir/$ARGV[0]";
-	$dest_dir = "$base_dir/$ARGV[1]";
-	return $source_dir, $dest_dir;
+	if (! (-e $vcluster_dir)){
+		mkdir "$base_dir/$vcluster_dir", 0755;
+	}
+	if ($num_clusters =~ /^-?\d+$/){
+		if ($num_clusters < 0){
+			print "Error: Number of clusters must be >= 1\n";
+			PrintUsageNotes();	
+			exit
+		}
+	}
+	else {
+		print "Error: Invalid input for number of clusters.\n";
+		PrintUsageNotes();
+		exit
+	}
+	$unformatted_dir = "$base_dir/$ARGV[0]";
+	$vcluster_dir = "$base_dir/$ARGV[1]";
+	return $unformatted_dir, $vcluster_dir, $num_clusters;
 }
 
-sub DetermineSizeofMatrices {
-	# determines size of matrix in each file
-	my %sizeof_matrices;
-	opendir my $dh, $source_dir or die "Can't open $source_dir: $!";
+sub SaveInputFilenames {
+	my @input_filenames;
+	opendir my $dh, $unformatted_dir or die "Can't open $unformatted_dir: $!";
 	while (my $file = readdir($dh)) {
 		next if ($file =~ /^\./);
 		next if -d $file;
-		if ($file =~ /.*?.cui.(\d+)/){
-			my $num_rows = $1;
-			my $num_columns;
-			open my $fh, '<', "$source_dir/$file" or die "Can't open $source_dir/$file: $!";
-			while (my $line = <$fh>){
-				if ($line =~ /^C\d{7}(.+)/){
-					$num_columns++;
-				}
-			}
-			close $fh;
-			$sizeof_matrices{$file}{$num_rows} = $num_columns;
-		}
+		push @input_filenames, $file;
 	}
 	closedir $dh;
+	return \@input_filenames;
+}
+
+sub GetSizeofMatrices {
+	# determines size of matrix in each file
+	my %sizeof_matrices;
+	foreach my $file (@$input_filenames){
+		open my $fh, '<', "$unformatted_dir/$file" or die "Can't open $unformatted_dir/$file: $!";
+		while (my $line = <$fh>){
+			if ($line =~ /^(-?\d+)\s(-?\d+)$/){
+				my $num_columns = $1;
+				my $num_rows = $2;
+				if ($num_clusters >= $num_rows){
+					print "Error: Number of clusters must be less than number of vectors ($file has $num_rows vector(s); you specified $num_clusters clusters).\n";
+					PrintUsageNotes();
+					exit
+				}
+				$sizeof_matrices{$file}{$num_rows} = $num_columns;
+				last;
+			}
+		}
+		close $fh;
+	}
 	return \%sizeof_matrices;
 }
 
 sub ExtractMatrices {
 	# extractors word2vec matrices from each file
 	my %matrices;
-	opendir my $dh, $source_dir or die "Can't open $source_dir: $!";
-	while (my $file = readdir($dh)) {
-		next if ($file =~ /^\./);
-		next if -d $file;
-		open my $fh, '<', "$source_dir/$file" or die "Can't open $source_dir/$file: $!";
-			$matrices{$file} = ();
-			while (my $line = <$fh>){
-				if ($line =~ /^C\d{7}(.+)/){
-					my $vector = $1;
-					my @vector_vals = ($vector =~ /(-?\d.\d+)/g);
-					push @{$matrices{$file}}, [@vector_vals];
-				}
+	foreach my $file (@$input_filenames){
+		$matrices{$file} = ();
+		open my $fh, '<', "$unformatted_dir/$file" or die "Can't open $unformatted_dir/$file: $!";
+		while (my $line = <$fh>){
+			if ($line =~ /^C\d{7}(.+)/){
+				my $vector = $1;
+				my @vector_vals = ($vector =~ /(-?\d.\d+)/g);
+				push @{$matrices{$file}}, [@vector_vals];
 			}
+		}
 		close $fh;
 	}
-	closedir $dh;
 	return \%matrices;
 }
 
@@ -83,7 +118,7 @@ sub PrintVClusterMatrices {
 	foreach my $file (keys %sizeof_matrices) {
 		foreach my $num_rows (keys $sizeof_matrices{$file}){
 			my $num_columns = $sizeof_matrices{$file}{$num_rows};
-			open my $fh, ">", "$dest_dir/$file" or die "Can't open $dest_dir/$file: $!";
+			open my $fh, ">", "$vcluster_dir/$file" or die "Can't open $vcluster_dir/$file: $!";
 			print $fh "$num_rows $num_columns\n";
 			foreach my $vector_vals (@{$matrices{$file}}) {
 				print $fh join(' ', @$vector_vals);
@@ -92,6 +127,32 @@ sub PrintVClusterMatrices {
 			close $fh;
 		}
 	}
-	print "Vcluster-formatted files located at: $dest_dir\n";
+	print "Vcluster-formatted files located at: $vcluster_dir\n";
+}
+
+sub SaveCuiVectorPairs {
+	my %cui_vector_lookup;
+	foreach my $file (@$input_filenames){
+		open my $fh, '<', "$unformatted_dir/$file" or die "Can't open $unformatted_dir/$file: $!";
+		while (my $line = <$fh>){
+			if ($line =~ /^(C\d{7})(.+)/){
+				my $cui = $1;
+				$cui_vector_lookup{$file}{$cui} = ();
+				my $vector = $2;
+				my @vector_vals = ($vector =~ /(-?\d.\d+)/g);
+				push @{$cui_vector_lookup{$file}{$cui}}, [@vector_vals];
+			}
+		}
+		close $fh;
+	}
+	return \%cui_vector_lookup;
+}
+
+sub RunVCluster {
+	#my $vcluster_command = `./vcluster $vcluster_dir/file $num_clusters`;
+}
+
+sub MatchClusteredVectorsToCuis {
+	#my %cui_vector_lookup = %$cui_vector_lookup;
 }
 
