0a81ed1df376b64274950cc1293e0323d0dd0d73
normanjw <normanj2@vcu.edu>
2017-09-24 11:36:55 -0400

    Update run_vcluster.pl

--- run_vcluster.pl
+++ run_vcluster.pl
@@ -3,63 +3,41 @@ use strict;
 use warnings;
 use Cwd;
 
-# usage: perl run_vcluster.pl [source_dir] [num_clusters] 
+# usage: perl run_vcluster.pl [source_dir] [num_clusters]
 
-my $base_dir = cwd();
-my ($input_dir, $num_clusters) = GetArgs();
-my $input_filenames = SaveInputFilenames();
-RunVCluster();
+my ($input_file, $num_clusters, $base_dir) = GetArgs();
+#RunVCluster();
 
 sub GetArgs {
 	# gets source directory and destination directory from user
-	no warnings 'uninitialized';;
-	my $input_dir = $ARGV[0];
+	no warnings 'uninitialized';
+	my $base_dir = cwd();
+	my $input_file = $ARGV[0];
 	my $num_clusters = $ARGV[1];
 
-	if (-f $input_dir){
-		print "Error: Input must be a directory.\n";
-		exit
-	}
-	if (! (-e $input_dir) ){
-		print "Error: $input_dir not found. Check source directory name.\n";
-		exit
-	}
-	if ($num_clusters =~ /(^-?\d+$)/){
-		if ($num_clusters < 0){
-			print "Error: Invalid input for number of clusters. Must be an integer > 0.\n";
-			exit
+	if (! -e $input_file || ! -f $input_file || $num_clusters < 0 || !($num_clusters =~ /(^-?\d+$)/)){
+		if (! -e $input_file){
+			print "Error: Input file $input_file not found.\n";
 		}
-	}
-	else {
-		print "Error: Invalid input for number of clusters. Must be an integer > 0.\n";
+		if (! -f $input_file){
+			print "Error: Invalid input for number of clusters. Must be an integer > 0.\n";
+		}
+		if ($num_clusters < 0){
+			print ""
+		}
+		if (! ($num_clusters =~ /(^-?\d+$)/)){
+			print "Error: Number of clusters must be an integer.\n";
+		}
+		print "Usage: perl run_vcluster.pl [source_dir] [num_clusters]\n";
 		exit
 	}
-	my $input_dir = "$base_dir/$input_dir";
-	return $input_dir, $num_clusters;
-}
-
-sub SaveInputFilenames {
-	my @input_filenames;
-	opendir my $dh, $input_dir or die "Can't open $input_dir: $!";
-	while (my $file = readdir($dh)) {
-		next if ($file =~ /^\./);
-		next if -d $file;
-		next if ($file =~ /\.clustering\./);
-		push @input_filenames, $file;
-	}
-	closedir $dh;
-
-	return \@input_filenames;
+	return $input_file, $num_clusters, $base_dir;
 }
 
 sub RunVCluster {
 	my $vcluster_dir = "$base_dir/cluto-2.1.1/Linux";
 	chdir $vcluster_dir;
-	foreach my $file (@$input_filenames){
-		my $vcluster_command = "./vcluster $vcluster_dir/$file $num_clusters";
-		system($vcluster_command);
-	}
+	my $vcluster_command = "./vcluster $input_file $num_clusters";
+	system($vcluster_command);
 	chdir;
 }
-
-
