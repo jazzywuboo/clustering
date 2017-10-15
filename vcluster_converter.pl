ca5bb6f3dfaff0b056f82538edb6e629f9b88c65
normanjw <normanj2@vcu.edu>
2017-10-15 19:11:10 -0400

    Update vcluster_converter.pl

--- vcluster_converter.pl
+++ vcluster_converter.pl
@@ -9,8 +9,8 @@ use Cwd;
 # output file ends in _v and is ready to be process by vcluster
 
 my $start_time = time;
-my ($cui_terms_file, $cui_vectors_file, $base_dir) = SetPaths();
-my $LTCs = SaveLTCs();
+my ($ltc_file, $vector_file, $base_dir) = SetPaths();
+my $linking_terms = SaveLinkingTerms();
 my $vectors = ExtractVectors();
 PrintVectors($vectors);
 my $end_time = time - $start_time;
@@ -18,15 +18,15 @@ my $execution_time = $end_time/60;
 printf("Execution time: %.2f mins\n", $execution_time);
 
 sub PrintUsageNotes {
-	print "Usage:\tperl vcluster_converter.pl [cui_terms_file] [cui_vectors_file]\n";
+	print "Usage:\tperl vcluster_converter.pl [ltc_file] [vector_file]\n";
 }
 
 sub SetPaths {
 	# gets source directory and destination directory from user
 	no warnings 'uninitialized';
 	my $base_dir = cwd();
-	my $cui_terms_file = "$ARGV[0]";
-	my $cui_vectors_file = "$ARGV[1]";
+	my $ltc_file = "$ARGV[0]";
+	my $vector_file = "$ARGV[1]";
 
 	if ($#ARGV < 1 || $#ARGV > 1){
 		print "Program takes 2 arguments.\n";
@@ -34,98 +34,91 @@ sub SetPaths {
 		exit
 	}
 
-	if (! -e $cui_terms_file || ! -f $cui_terms_file || ! -e $cui_vectors_file || ! -f $cui_vectors_file){
-		if (! -e $cui_terms_file){
-			print "Error: $cui_terms_file not found.\n";
+	if (! -e $ltc_file || ! -f $ltc_file || ! -e $vector_file || ! -f $vector_file){
+		if (! -e $ltc_file){
+			print "Error: $ltc_file not found.\n";
 		}
-		if (! -f $cui_terms_file){
-			print "Error: $cui_terms_file must be a file.\n";
+		if (! -f $ltc_file){
+			print "Error: $ltc_file must be a file.\n";
 		}
-		if (! -e $cui_vectors_file){
-			print "Error: $cui_vectors_file not found.\n";
+		if (! -e $vector_file){
+			print "Error: $vector_file not found.\n";
 		}
-		if (! -f $cui_vectors_file){
-			print "Error: $cui_vectors_file must be a file.\n";
+		if (! -f $vector_file){
+			print "Error: $vector_file must be a file.\n";
 		}
 		PrintUsageNotes();
 		exit
 	}
-	return $cui_terms_file, $cui_vectors_file, $base_dir;
+	return $ltc_file, $vector_file, $base_dir;
 }
 
-sub SaveLTCs {
-	# saves cui-term-score information in a hash of structure %LTCs: cui -> term -> score
-	my %LTCs;
-	open my $fh, '<', "$base_dir/$cui_terms_file" or die "Can't open $base_dir/$cui_terms_file: $!";
+sub SaveLinkingTerms {
+	# saves cui-term-score information in a hash of structure %linking_terms: cui -> term -> score
+	my %linking_terms;
+	open my $fh, '<', "$base_dir/$ltc_file" or die "Can't open $base_dir/$ltc_file: $!";
 	while (my $line = <$fh>) {
-		if ($line =~ /^\d+\t\d+.\d+\t(C\d{7})\t.+/){
-			my $cui = $1;
-			$LTCs{$cui} = 1;	# just using '1' as null value
+		if ($line =~ /\d+\t(\d+.\d+)\t(C\d{7})\t.+/){
+			my $score = $1;
+			my $cui = $2;
+			$linking_terms{$cui} = $score;
 		}
 	}
 	close $fh;
-	return \%LTCs;
+	return \%linking_terms;
 }
 
 sub ExtractVectors {
 	# extractors word2vec vectors that correspond to LTC cui's and saves to an array of arrays
-	my @vectors;
-	my %LTCs = %$LTCs;
-	open my $fh, '<', "$base_dir/$cui_vectors_file" or die "Can't open $base_dir/$cui_vectors_file: $!";
+	my %vectors;
+	my %linking_terms = %$linking_terms;
+	open my $fh, '<', "$base_dir/$vector_file" or die "Can't open $base_dir/$vector_file: $!";
 	while (my $line = <$fh>){
 		if ($line =~ /^(C\d{7})(.+)/){
 			my $cui = $1;
-			if (exists $LTCs{$cui}){
+			if (exists $linking_terms{$cui}){
 				my $vector = $2;
 				my @vector_vals = ($vector =~ /(-?\d.\d+)/g);
-				push @vectors, [@vector_vals];
+				$vectors{$cui} = [@vector_vals];
 			}
 		}
 	}
 	close $fh;
-	return \@vectors;
+	return \%vectors;
 }
 
-sub TestCuis {
-	my %test_cuis;
-	my %LTCs = %$LTCs;
-	open my $fh, '<', "$base_dir/$cui_vectors_file" or die "Can't open $base_dir/$cui_vectors_file: $!";
-	while (my $line = <$fh>){
-		if ($line =~ /^(C\d{7})(.+)/){
-			my $cui = $1;
-			my $vector = $2;
-			my @vector_vals = ($vector =~ /(-?\d.\d+)/g);
-			$test_cuis{$cui} = $1;
-		}
+sub TestSort {
+	my %linking_terms = %$linking_terms;
+	my %ranked_cuis;
+	my @cuis = sort {$linking_terms{$b} <=> $linking_terms{$a}} keys(%linking_terms);
+	foreach my $cui (@cuis){
+		my $score = $linking_terms{$cui};
+		$ranked_cuis{$cui} = $score;
+		print "$cui $score\n";
 	}
-	close $fh;
-	my $found = 0;
-	my $not_found = 0;
-	foreach my $cui (keys %LTCs){
-		if (exists $test_cuis{$cui}){
-			$found++;
-		}
-		else {
-			$not_found++;
-		}
-	}
-	print "$found found, $not_found not found.\n";
 }
 
 sub PrintVectors {
 	# prints dense matrices in format required for CLUTO's vcluster program
-	my @vectors = @$vectors;
-	my $num_rows = @vectors;
-	my $num_columns = @{$vectors[0]};
+	my %vectors = %$vectors;
+	my $num_rows = keys %vectors;
+	my $num_columns = 200;		# change this later
 	my $file_extension = "_v";
-	my $vcluster_file = "$cui_vectors_file$file_extension";
+	my %linking_terms = %$linking_terms;
+	my $vcluster_file = "$vector_file$file_extension";
+
 	open my $fh, ">", "$base_dir/$vcluster_file" or die "Can't open $base_dir/$vcluster_file: $!";
 	print $fh "$num_rows $num_columns\n";
-	foreach my $vector (@vectors) {
-		print $fh join(' ', @$vector);
-		print $fh "\n";
+
+	my @cuis = sort {$linking_terms{$b} <=> $linking_terms{$a}} keys(%linking_terms);	# prints vectors sorted by ltc score
+	foreach my $cui (@cuis){
+		if (exists $vectors{$cui}){
+			my $vector = @$vectors{$cui};
+			print $fh join(' ', @$vector);
+			print $fh "\n";
+		}
 	}
 	close $fh;
+
 	print "Vcluster-formatted files located at: $base_dir/$vcluster_file\n";
 }
-
