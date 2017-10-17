4a4d306021045cd77b11ad64084a32e76f258b3c
normanjw <normanj2@vcu.edu>
2017-10-17 12:09:04 -0400

    Update run_clustering.pl

--- run_clustering.pl
+++ run_clustering.pl
@@ -1,29 +1,27 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use Cwd;
 
 my $start_time = time;
 
 my $ltc_file = $ARGV[0];
 my $vector_file = $ARGV[1];
-my $num_clusters = $ARGV[2];
-my $vcluster_path = $ARGV[3];
-my $opt_data_dir = $ARGV[4];
+my $vcluster_path = $ARGV[2];
+my $opt_data_dir = $ARGV[3];
 
-if ($#ARGV < 4 || $#ARGV > 5) {
-	print "Program requires 3 arguments to run; 4th is optional.\n";
-	print "Usage: perl run_clustering.pl [ltc_file] [vector_file] [num_clusters] [vcluster_path] [opt_data_dir]\n";
+if ($#ARGV < 3 || $#ARGV > 4) {
+	print "Program requires 2 arguments to run; 3rd is optional.\n";
+	print "Usage: perl run_clustering.pl [ltc_file] [vector_file] [vcluster_path] [opt_data_dir]\n";
 	exit
 }
 
 my $reduced_ltc_file = $ltc_file . '_reduced';
 my $reduced_vector_file = $vector_file . '_v';
-my $clustered_file = $reduced_vector_file . '.clustering.' . $num_clusters;
 
-for (my $i = 0; $i <= 1; $i += 0.05){
+for (my $num_clusters = 0; $num_clusters <= 1; $num_clusters += 0.05){
 	`perl vcluster_converter.pl $ltc_file $vector_file $opt_data_dir`;
 	`perl run_vcluster.pl $vcluster_path $reduced_vector_file $num_clusters $opt_data_dir`;
+	my $clustered_file = $reduced_vector_file . '.clustering.' . $num_clusters;
 	`perl label_and_rank_clusters.pl $reduced_ltc_file $reduced_vector_file $clustered_file $opt_data_dir`;
 }
 my $end_time = time - $start_time;
